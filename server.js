0584c8c163314e82643c6d566ec89aa97cddc1f6
Trevor Mackin <tismetrm@gmail.com>
2021-04-27 15:47:18 -0700

    OAuth 2.0 using a google example

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// It is important to config env at top of file and ASAP
-// It only needs to require and config once, so I won't save to a variable
 require("dotenv").config();
 const express = require("express");
 const _ = require("lodash");
@@ -10,14 +8,16 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
-// Express v4.16.0 and higher
+//? Express v4.16.0 and higher
 // --------------------------
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// For Express version less than 4.16.0
+//? For Express version less than 4.16.0
 // ------------------------------------
 // const bodyParser = require('body-parser');
 // app.use(express.json());
@@ -29,9 +29,9 @@ app.use(express.static("public"));
 
 // Cookies and Sessions (express-session)
 app.use(session({
-  secret: "Our little secret.",
-  resave: false,
-  saveUninitialized: false
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
 }));
 
 // (passport)
@@ -50,63 +50,80 @@ app.listen(port, function () {
 
 // Connect to database, create schema, and encrypt password
 mongoose.connect("mongodb://localhost:27017/userDB", {
-     useNewUrlParser: true,
-     useUnifiedTopology: true,
-     useCreateIndex: true,
-     //useFindAndModify: false, 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    //useFindAndModify: false, 
 });
 
 const userSchema = new mongoose.Schema({
-    email: String, 
-    password: String
+    email: String,
+    password: String,
+    googleId: String,
+    secret: String,
 });
 
 // This must be used with a mongoose schema instance (not just a js object)
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:5000/auth/google/secrets",
+    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
+},
+    function (accessToken, refreshToken, profile, cb) {
+        //! IMPORTANT: You must install the mongoose-findorcreate package, require it into the 
+        //! module it's being called in, and then add as plugin to the userSchema before calling. 
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
 // Routes
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.render("home");
 });
 
-app.get("/login", function(req, res) {
-    res.render("login");
-});
-
-app.get("/register", function(req, res) {
+app.get("/register", function (req, res) {
     res.render("register");
 });
 
-app.get("/secrets", function (req, res) {
-    if (req.isAuthenticated()) {
-        res.render("secrets");
-    }
-    else {
-        res.redirect("/login");
-    }
-})
-
-app.post("/register", function(req, res) {
-    User.register({username: req.body.username}, req.body.password, function(err, user) {
+app.post("/register", function (req, res) {
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
         if (err) {
             console.log(err);
             res.redirect("/register");
         }
         else {
-            passport.authenticate("local")(req, res, function() {
+            passport.authenticate("local")(req, res, function () {
                 res.redirect("/secrets");
             });
         }
-    })
+    });
 });
 
-app.post("/login", function(req, res) {
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.post("/login", function (req, res) {
 
     const user = new User({
         username: req.body.username,
@@ -125,7 +142,72 @@ app.post("/login", function(req, res) {
     });
 });
 
+app.get("/auth/google",
+    passport.authenticate("google", { scope: ["profile"] }),
+);
+
+app.get("/auth/google/secrets",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    function (req, res) {
+        // Successful authentication, redirect to secrets.
+        res.redirect("/secrets");
+    }
+);
+
+app.get("/secrets", function (req, res) {
+    User.find({ "secret": { $ne: null } }, function (err, foundUsers) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUsers) {
+                res.render("secrets", { usersWithSecrets: foundUsers });
+            }
+        }
+    });
+    //! If you only want registered users to see the secrets list, then implement some sort of auth
+    // if (req.isAuthenticated()) {
+    //     res.render("secrets");
+    // }
+    // else {
+    //     res.redirect("/login");
+    // }
+});
+
+app.get("/submit", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("submit");
+    }
+    else {
+        res.redirect("/login");
+    }
+});
+
+app.post("/submit", function (req, res) {
+    const usersSubmittedSecret = req.body.secret;
+    // find who current user is (always saved to req variable);
+    User.findById({ _id: req.user._id }, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            if (foundUser) {
+                foundUser.secret = usersSubmittedSecret;
+                foundUser.save(function (err) {
+                    if (err) {
+                        res.send(err);
+                    } else {
+                        res.redirect("/secrets");
+                    }
+                });
+            }
+        }
+    });
+});
+
 app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
 });
+
+//! OAuth redirect URI
+// http://localhost:5000/auth/google/secrets
